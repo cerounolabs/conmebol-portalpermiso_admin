1468da0413a1aebeb6b660b21d8c54df76ceac82
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2019-11-20 11:13:33 -0300

    Feature LISTAR COLABORADORES

---
+++ public/colaborador.php
@@ -0,0 +1,163 @@
+<?php
+    require '../class/function/curl_api.php';
+    require '../class/function/function.php';
+    require '../class/session/session_system.php';
+
+    if(isset($_GET['code'])){
+        $codeRest       = $_GET['code'];
+        $msgRest        = $_GET['msg'];
+    } else {
+        $codeRest       = 0;
+        $msgRest        = '';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+
+<head>
+<?php
+    include '../include/header.php';
+?>
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+<?php
+    	include '../include/menu.php';
+?>
+       
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-5 align-self-center">
+                        <div class="d-flex align-items-center"></div>
+                    </div>
+                    <div class="col-7 align-self-center">
+                        <div class="d-flex no-block justify-content-end align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                        <a href="../public/home.php">Home</a>
+                                    </li>
+                                    <li class="breadcrumb-item active" aria-current="page">Colaborador</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <!-- basic table -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <h4 class="col-10 card-title">Colaboradores</h4>
+								</div>
+                                <div class="table-responsive">
+                                    <table id="tableLoad" class="table v-middle" style="width: 100%;">
+                                        <thead id="tableCodigo" class="">
+                                            <tr class="bg-light">
+                                                <th class="border-top-0">NRO. DOCUMENTO</th>
+                                                <th class="border-top-0">COLABORADOR</th>
+                                                <th class="border-top-0">EMAIL</th>
+                                                <th class="border-top-0">CARGO</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Modal Procesar -->
+                <div id="modalprocesar" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="vcenter" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" id="prodesc">
+                    </div>
+                </div>
+                <!-- Modal Procesar -->
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+<?php
+    include '../include/development.php';
+?>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <div class="chat-windows"></div>
+<?php
+    include '../include/footer.php';
+   
+    if ($codeRest === 200) {
+?>
+    <script>
+        $(function() {
+            toastr.success('<?php echo $msgRest; ?>', 'Correcto!');
+        });
+    </script>
+<?php
+    }
+            
+    if (($codeRest === 204) || ($codeRest === 401)) {
+?>
+    <script>
+        $(function() {
+            toastr.error('<?php echo $msgRest; ?>', 'Error!');
+        });
+    </script>
+<?php
+    }
+?>
+
+    <script src="../js/colaborador.js"></script>
+</body>
+</html>
