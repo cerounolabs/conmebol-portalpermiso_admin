b6b0b18dbb650dbcdef66e1330eb59426ea29460
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2019-11-20 16:00:42 -0300

    Feature LISTAR COLABORADORES

---
+++ js/solicitudes.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+	var codigo		= document.getElementById('tableCodigo').className;	
+	var urlDominio	= 'http://api.conmebol.com/portalpermiso/public/v1/200/colaborador/'+codigo;
+
+	$('#tableLoad').DataTable({
+		processing	: true,
+		destroy		: true,
+		searching	: true,
+		paging		: true,
+		lengthChange: true,
+		info		: true,
+		orderCellsTop: false,
+        fixedHeader	: false,
+		language	: {
+            lengthMenu: "Mostrar _MENU_ registros por pagina",
+            zeroRecords: "Nothing found - sorry",
+            info: "Mostrando pagina _PAGE_ de _PAGES_",
+            infoEmpty: "No hay registros disponibles.",
+			infoFiltered: "(Filtrado de _MAX_ registros totales)",
+			sZeroRecords: "No se encontraron resultados",
+			sSearch: "buscar",
+			oPaginate: {
+				sFirst:    "Primero",
+				sLast:     "Último",
+				sNext:     "Siguiente",
+				sPrevious: "Anterior"
+			},
+        },
+		ajax		: {
+			type				: 'GET',
+			cache				: false,
+			crossDomain			: true,
+			crossOrigin			: true,
+			contentType			: 'application/json; charset=utf-8',
+			dataType			: 'json',
+			url				: urlDominio,
+			dataSrc				: 'data'
+		},
+		columnDefs	: [
+			{ targets			: [0],	visible : true,	searchable : true,	orderData : [0, 0] },
+			{ targets			: [1],	visible : true,	searchable : true,	orderData : [1, 0] },
+			{ targets			: [2],	visible : true,	searchable : true,	orderData : [2, 0] },
+			{ targets			: [3],	visible : true,	searchable : true,	orderData : [3, 0] },
+			{ targets			: [4],	visible : true,	searchable : true,	orderData : [4, 0] },
+			{ targets			: [5],	visible : true,	searchable : true,	orderData : [5, 0] }
+		],
+		columns		: [
+			{ data				: 'documento', name : 'documento'},
+			{ data				: 'nombre_completo', name : 'nombre_completo'},
+			{ data				: 'email', name : 'email'},
+			{ data				: 'cargo_nombre', name : 'cargo_nombre'},
+			{ data				: 'cargo_nombre', name : 'cargo_nombre'},
+			{ data				: 'cargo_nombre', name : 'cargo_nombre'},
+		]
+	});
+});
---
+++ public/solicitudes.php
@@ -0,0 +1,252 @@
+<?php
+    require '../class/function/curl_api.php';
+    require '../class/function/function.php';
+    require '../class/session/session_system.php';
+
+    if(isset($_GET['code'])){
+        $codeRest       = $_GET['code'];
+        $msgRest        = $_GET['msg'];
+    } else {
+        $codeRest       = 0;
+        $msgRest        = '';
+    }
+
+    $solictudJSON = get_curl('100');
+?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+
+<head>
+<?php
+    include '../include/header.php';
+?>
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+<?php
+    	include '../include/menu.php';
+?>
+       
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-5 align-self-center">
+                        <div class="d-flex align-items-center"></div>
+                    </div>
+                    <div class="col-7 align-self-center">
+                        <div class="d-flex no-block justify-content-end align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                        <a href="../public/home.php">Home</a>
+                                    </li>
+                                    <li class="breadcrumb-item active" aria-current="page">Solicitudes</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <!-- basic table -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <h4 class="col-10 card-title">Solicitudes</h4>
+                                    <h4 class="col-2 card-title" style="text-align: right;">
+                                        <a class="btn btn-info" style="background-color:#163562; border-color:#163562;"  href="javascript:void(0)" role="button" title="Solicitud" data-toggle="modal" data-target="#modaldiv" onclick="setSolicitud();"><i class="ti-plus"></i></a>
+                                	</h4>
+								</div>
+                                <div class="table-responsive">
+                                    <table id="tableLoad" class="table v-middle" style="width: 100%;">
+                                        <thead id="tableCodigo" class="<?php echo $usu_05; ?>">
+                                            <tr class="bg-light">
+                                                <th class="border-top-0">ESTADO</th>
+                                                <th class="border-top-0">SOLICITUD</th>
+                                                <th class="border-top-0">DESDE</th>
+                                                <th class="border-top-0">HASTA</th>
+                                                <th class="border-top-0">APROBADOR POR</th>
+                                                <th class="border-top-0"></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Modal Procesar -->
+                <div id="modaldiv" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="vcenter" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" id="modalcontent">
+                    </div>
+                </div>
+                <!-- Modal Procesar -->
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+<?php
+    include '../include/development.php';
+?>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <div class="chat-windows"></div>
+<?php
+    include '../include/footer.php';
+   
+    if ($codeRest === 200) {
+?>
+    <script>
+        $(function() {
+            toastr.success('<?php echo $msgRest; ?>', 'Correcto!');
+        });
+    </script>
+<?php
+    }
+            
+    if (($codeRest === 204) || ($codeRest === 401)) {
+?>
+    <script>
+        $(function() {
+            toastr.error('<?php echo $msgRest; ?>', 'Error!');
+        });
+    </script>
+<?php
+    }
+?>
+
+    <script src="../js/solicitudes.js"></script>
+
+    <script>
+        function setSolicitud(){
+            var html    =
+            '<div class="modal-content">'+
+            '   <form id="form" data-parsley-validate method="post" action="">'+
+            '	    <div class="modal-header" style="color:#fff; background:#163562;">'+
+            '		    <h4 class="modal-title" id="vcenter"> Solicitud </h4>'+
+            '		    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>'+
+            '	    </div>'+
+            '	    <div class="modal-body" >'+
+            '           <div class="form-group">'+
+            '               <input id="workCodigo" name="workCodigo" value="" class="form-control" type="hidden" placeholder="Codigo" required readonly>'+
+            '               <input id="workModo" name="workModo" value="U" class="form-control" type="hidden" placeholder="Modo" required readonly>'+
+            '           </div>'+
+            '           <div class="row pt-3">'+
+            '               <div class="col-sm-12">'+
+            '                   <div class="form-group">'+
+            '                       <label for="var01">SOLICITUD DE</label>'+
+            '                       <select id="var01" name="var01" class="select2 form-control custom-select" style="width:100%; height:40px;">'+
+            '                           <optgroup label="Solicitud">'+
+<?php
+    if ($solictudJSON['code'] === 200) {
+        foreach ($solictudJSON['data'] as $solictudKEY => $solictudVALUE) {
+            if ($solictudVALUE['tipo_estado_codigo'] === 'A' && $solictudVALUE['tipo_solicitud_codigo'] != 'I'){
+?>
+            '                               <option value="<?php echo $solictudVALUE['tipo_permiso_codigo']; ?>"><?php echo $solictudVALUE['tipo_solicitud_nombre'].' - '.$solictudVALUE['tipo_permiso_nombre']; ?></option>'+
+<?php
+            }
+        }
+    }
+?>
+            '                           </optgroup>'+
+            '                       </select>'+
+            '                   </div>'+
+            '               </div>'+
+            '           </div>'+
+            '           <div class="row pt-3">'+
+            '               <div class="col-sm-12 col-md-3">'+
+            '                   <div class="form-group">'+
+            '                       <label for="var02">FECHA DESDE</label>'+
+            '                       <input id="var02" name="var02" class="form-control" type="date" style="text-transform:uppercase; height:40px;" placeholder="FECHA DESDE">'+
+            '                   </div>'+
+            '               </div>'+
+            '               <div class="col-sm-12 col-md-3">'+
+            '                   <div class="form-group">'+
+            '                       <label for="var03">FECHA HASTA</label>'+
+            '                       <input id="var03" name="var03" class="form-control" type="date" style="text-transform:uppercase; height:40px;" placeholder="FECHA HASTA">'+
+            '                   </div>'+
+            '               </div>'+
+            '               <div class="col-sm-12 col-md-3">'+
+            '                   <div class="form-group">'+
+            '                       <label for="var04">HORA DESDE</label>'+
+            '                       <input id="var04" name="var04" class="form-control" type="time" style="text-transform:uppercase; height:40px;" placeholder="FECHA HASTA">'+
+            '                   </div>'+
+            '               </div>'+
+            '               <div class="col-sm-12 col-md-3">'+
+            '                   <div class="form-group">'+
+            '                       <label for="var05">HORA HASTA</label>'+
+            '                       <input id="var05" name="var05" class="form-control" type="time" style="text-transform:uppercase; height:40px;" placeholder="FECHA HASTA">'+
+            '                   </div>'+
+            '               </div>'+
+            '           </div>'+
+            '           <div class="row pt-3">'+
+            '                <div class="col-sm-12">'+
+            '                    <div class="form-group">'+
+            '                        <label for="var05">COMENTARIO</label>'+
+            '                        <textarea id="var05" name="var05" class="form-control" rows="3" style="text-transform:uppercase;"></textarea>'+
+            '                    </div>'+
+            '                </div>'+
+            '           </div>'+
+            '	    </div>'+
+            '	    <div class="modal-footer">'+
+            '           <button type="submit" class="btn btn-info">Guardar</button>'+
+            '		    <button type="button" class="btn btn-dark" data-dismiss="modal">Cerrar</button>'+
+            '	    </div>'+
+            '   </form>'+
+            '</div>';
+            $("#modalcontent").empty();
+            $("#modalcontent").append(html);
+        }
+    </script>
+</body>
+</html>
