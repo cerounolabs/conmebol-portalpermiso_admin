b06d941821921bb4248394b9d2ddc467eee1dece
Christian Zelaya <christian@cerouno.com.py>
2019-12-17 09:45:02 -0300

    Update SOLICITUDES

--- public/solicitudes.php
+++ public/solicitudes.php
@@ -11,7 +11,8 @@
         $msgRest        = '';
     }
 
-    $solictudJSON = get_curl('100');
+    $solictudJSON   = get_curl('100');
+    $solictudesJSON = get_curl('200/solicitud/'.$usu_05);
 ?>
 
 <!DOCTYPE html>
@@ -168,10 +169,58 @@
 <?php
     }
 ?>
-
-    <script src="../js/solicitudes.js"></script>
-
     <script>
+        $(document).ready(function() {
+            $('#tableLoad').DataTable({
+                processing	: true,
+                destroy		: true,
+                searching	: true,
+                paging		: true,
+                lengthChange: true,
+                info		: true,
+                orderCellsTop: false,
+                fixedHeader	: false,
+                language	: {
+                    lengthMenu: "Mostrar _MENU_ registros por pagina",
+                    zeroRecords: "Nothing found - sorry",
+                    info: "Mostrando pagina _PAGE_ de _PAGES_",
+                    infoEmpty: "No hay registros disponibles.",
+                    infoFiltered: "(Filtrado de _MAX_ registros totales)",
+                    sZeroRecords: "No se encontraron resultados",
+                    sSearch: "buscar",
+                    oPaginate: {
+                        sFirst:    "Primero",
+                        sLast:     "Último",
+                        sNext:     "Siguiente",
+                        sPrevious: "Anterior"
+                    },
+                },
+                data		: <?php echo json_encode($solictudesJSON['data']); ?>,
+                columnDefs	: [
+                    { targets			: [0],	visible : true,	searchable : true,	orderData : [0, 0] },
+                    { targets			: [1],	visible : true,	searchable : true,	orderData : [1, 0] },
+                    { targets			: [2],	visible : true,	searchable : true,	orderData : [2, 0] },
+                    { targets			: [3],	visible : true,	searchable : true,	orderData : [3, 0] },
+                    { targets			: [4],	visible : true,	searchable : true,	orderData : [4, 0] },
+                    { targets			: [5],	visible : true,	searchable : true,	orderData : [5, 0] },
+                    { targets			: [6],	visible : true,	searchable : true,	orderData : [6, 0] },
+                    { targets			: [7],	visible : true,	searchable : true,	orderData : [7, 0] },
+                    { targets			: [8],	visible : true,	searchable : true,	orderData : [8, 0] }
+                ],
+                columns		: [
+                    { data				: 'solicitud_estado_nombre', name : 'solicitud_estado_nombre'},
+                    { data				: 'solicitud_documento', name : 'solicitud_documento'},
+                    { data				: 'solicitud_persona', name : 'solicitud_persona'},
+                    { data				: 'tipo_permiso_nombre', name : 'tipo_permiso_nombre'},
+                    { data				: 'solicitud_fecha_cantidad', name : 'solicitud_fecha_cantidad'},
+                    { data				: 'solicitud_hora_cantidad', name : 'solicitud_hora_cantidad'},
+                    { data				: 'solicitud_usuario_aprobador', name : 'solicitud_usuario_aprobador'},
+                    { data				: 'solicitud_usuario_talento', name : 'solicitud_usuario_talento'},
+                    { render			: function (data, type, full, meta) {return '<a href="javascript:void(0)" id="'+ full.solicitud_codigo +'" value="'+ full.solicitud_estado_codigo +'" value2="'+ full.solicitud_documento +'" role="button" class="btn btn-success" title="Autorizar/Anular" data-toggle="modal" data-target="#modaldiv" onclick="setEstado1(this.id);"><i class="ti-settings"></i>&nbsp;</a>&nbsp;<a href="javascript:void(0)" id="'+ full.solicitud_codigo +'" value="'+ full.solicitud_estado_codigo +'" value2="'+ full.solicitud_documento +'" role="button" class="btn btn-warning" title="Aprobar/Anular" data-toggle="modal" data-target="#modaldiv" onclick="setEstado2(this.id);"><i class="ti-medall-alt"></i>&nbsp;</a>&nbsp;';}},
+                ]
+            });
+        });
+
         function cantFecha(){
             var fecDesde    = document.getElementById('var02');
             var fecHasta    = document.getElementById('var03');
