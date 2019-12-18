222c6d2b0e882a383448b82b66ebb6261b8504a9
Christian Zelaya <christian@cerouno.com.py>
2019-12-18 10:28:37 -0300

    Fix Workflow DASHBOARD

--- js/cargo.js
+++ js/cargo.js
@@ -37,9 +37,9 @@ $(document).ready(function() {
 			dataSrc				: 'data'
 		},
 		columnDefs	: [
-			{ targets			: [0],	visible : false,searchable : false,	orderData : [0, 0] },
-			{ targets			: [1],	visible : false,searchable : false,	orderData : [1, 0] },
-			{ targets			: [2],	visible : false,searchable : false,	orderData : [2, 0] },
+			{ targets			: [0],	visible : true,searchable : true,	orderData : [0, 0] },
+			{ targets			: [1],	visible : true,searchable : true,	orderData : [1, 0] },
+			{ targets			: [2],	visible : true,searchable : true,	orderData : [2, 0] },
 			{ targets			: [3],	visible : true,	searchable : true,	orderData : [3, 0] },
 			{ targets			: [4],	visible : true,	searchable : true,	orderData : [4, 0] },
 			{ targets			: [5],	visible : true,	searchable : true,	orderData : [5, 0] },
