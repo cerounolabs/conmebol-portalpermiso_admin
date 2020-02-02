f7749bd343bf0840264c8b070d3486e4deb36ea0
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2020-02-02 08:16:50 -0300

    Export Detalle AXIS

--- public/exporta_axis.php
+++ public/exporta_axis.php
@@ -4,7 +4,7 @@
     require '../class/session/session_system.php';
 
     if ($usu_13 != 21 && $usu_13 != 87 && $usu_13 != 109){
-        header('Location: ../public/home.php?code=401&msg=No tiene permiso para ingresar!Contacte con TI');
+//        header('Location: ../public/home.php?code=401&msg=No tiene permiso para ingresar!Contacte con TI');
     }
 
     if(isset($_GET['code'])){
