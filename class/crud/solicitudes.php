3128efb0b19167b233aaac24a64fad7e75741442
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2020-03-12 21:31:36 -0300

    Prueba UPLOAD

--- class/crud/solicitudes.php
+++ class/crud/solicitudes.php
@@ -6,8 +6,7 @@
     ob_start();
     
 	require '../../class/function/curl_api.php';
-
-	print_r($_FILES['var08']);
+	require '../../class/function/function.php';
 
     $val01          = $_POST['var01'];
     $val02          = $_POST['var02'];
@@ -16,23 +15,27 @@
 	$val05          = $_POST['var05'];
 	$val06          = $_POST['var06'];
 	$val07          = $_POST['var07'];
-	$val08          = strtolower($_POST['var08']);
+	$val08          = '';
 	$val09          = strtoupper($_POST['var09']);
+	$target_ban     = true;
 
-	if (isset($val08)) {
+	if (isset($_FILES['var08'])) {
+		$target_ban     = false;
+        $target_msn     = '';
+		$target_nam     = getFechaHora();
 		$target_dir     = '../../uploads/';
         $target_file    = $target_dir.basename($_FILES['var08']['name']);
-        $target_ban     = false;
-        $target_msn     = '';
-        $target_nam     = '';
-        $imageFileType  = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        
+		$imageFileType  = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+		$target_file	= $target_nam.'.'.$imageFileType;
+
         if(isset($_POST['submit'])) {
-            $check = getimagesize($_FILES['var08']['tmp_name']);
+			$check = getimagesize($_FILES['var08']['tmp_name']);
+
             if($check !== false) {
-                $target_ban = false;
+				$target_ban = true;
             } else {
-                $target_ban = true;
+				$target_ban = false;
+				$target_msn = 'ERROR: El archivo no es correcto. Verifique!';
             }
         }
         
@@ -52,64 +55,69 @@
         }
 
         if ($target_ban == true) {
-            if (move_uploaded_file($_FILES['var08']['tmp_name'], $target_file)) {
-                $target_msn = 'SUCCESS: Ok';
-                $target_nam = $target_dir.''.$_FILES['var08']['tmp_name'];
+            if (move_uploaded_file($_FILES['var08']['tmp_name'], $target_dir.''.$target_file)) {
+				$val08		= $target_file;
+				$target_msn	= $target_file;
                 $target_ban = true;
             } else {
                 $target_msn = 'ERROR: El archivo tuvo inconveniente en subir, favor intente devuelta. Verifique!';
                 $target_ban = false;
             }
-        }
-	}
-
-    $work01         = $_POST['workCodigo'];
-	$work02         = $_POST['workModo'];
-	$work03         = $_POST['workPage'];
-
-	$usu_03         = strtoupper($_SESSION['usu_03']);
-	$usu_05         = strtoupper($_SESSION['usu_05']);
-
-	$log_03         = $_SESSION['log_03'];
-
-    if (isset($val01) && isset($val02) && isset($val03)) {
-        $dataJSON = json_encode(
-            array(
-				'tipo_estado_codigo'				=> 'I',
-				'tipo_solicitud_codigo'				=> $val01,
-				'solicitud_documento'				=> $usu_05,
-				'solicitud_fecha_desde'				=> $val02,
-				'solicitud_fecha_hasta'				=> $val03,
-                'solicitud_fecha_cantidad'			=> $val04,
-				'solicitud_hora_desde'				=> $val05,
-				'solicitud_hora_hasta'				=> $val06,
-				'solicitud_hora_cantidad'			=> $val07,
-				'solicitud_adjunto'					=> $val08,
-				'solicitud_observacion_colaborador'	=> $val09,
-				'auditoria_usuario'     			=> $usu_03,
-                'auditoria_fecha_hora'  			=> date('Y-m-d H:i:s'),
-                'auditoria_ip'          			=> $log_03
-			));
-/*		
-		switch($work02){
-			case 'C':
-				$result	= post_curl('200', $dataJSON);
-				break;
-			case 'U':
-				$result	= put_curl('200/'.$work01, $dataJSON);
-				break;
-			case 'D':
-				$result	= delete_curl('200/'.$work01, $dataJSON);
-				break;
 		}
-	*/
+		
+		$code	= 400;
+		$message= $target_msn;
 	}
 
-	echo json_encode($dataJSON);
-/*
-	$result		= json_decode($result, true);
+	if ($target_ban == true) {
+		$work01         = $_POST['workCodigo'];
+		$work02         = $_POST['workModo'];
+		$work03         = $_POST['workPage'];
 
-	header('Location: ../../public/'.$work03.'code='.$result['code'].'&msg='.$result['message']);
+		$usu_03         = strtoupper($_SESSION['usu_03']);
+		$usu_05         = strtoupper($_SESSION['usu_05']);
+
+		$log_03         = $_SESSION['log_03'];
+
+		if (isset($val01) && isset($val02) && isset($val03)) {
+			$dataJSON = json_encode(
+				array(
+					'tipo_estado_codigo'				=> 'I',
+					'tipo_solicitud_codigo'				=> $val01,
+					'solicitud_documento'				=> $usu_05,
+					'solicitud_fecha_desde'				=> $val02,
+					'solicitud_fecha_hasta'				=> $val03,
+					'solicitud_fecha_cantidad'			=> $val04,
+					'solicitud_hora_desde'				=> $val05,
+					'solicitud_hora_hasta'				=> $val06,
+					'solicitud_hora_cantidad'			=> $val07,
+					'solicitud_adjunto'					=> $val08,
+					'solicitud_observacion_colaborador'	=> $val09,
+					'auditoria_usuario'     			=> $usu_03,
+					'auditoria_fecha_hora'  			=> date('Y-m-d H:i:s'),
+					'auditoria_ip'          			=> $log_03
+				));
+/*					
+			switch($work02){
+				case 'C':
+					$result	= post_curl('200', $dataJSON);
+					break;
+				case 'U':
+					$result	= put_curl('200/'.$work01, $dataJSON);
+					break;
+				case 'D':
+					$result	= delete_curl('200/'.$work01, $dataJSON);
+					break;
+			}
 */
+		}
+
+//		$result = json_decode($result, true);
+//		$code	= $result['code'];
+//		$message= $result['message'];
+	}
+
+	header('Location: ../../public/'.$work03.'code='.$code.'&msg='.$message);
+
 	ob_end_flush();
 ?>
