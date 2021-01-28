b41e2fce2347c7a310e2e3386ff38eaf46740c04
CEROUNO_PC01 <christian@cerouno.com.py>
2021-01-28 11:24:02 -0300

    Ajuste 202101272157

--- js/perfil.js
+++ js/perfil.js
@@ -20,7 +20,7 @@ function getVCARD(codDoc){
         xJSON2.forEach(element1 => {
             if(element1.tarjeta_personal_codigo == element.tarjeta_personal_codigo && element1.tarjeta_personal_telefono_visualizar == 'S'){
                 vcardTEL= element1.tarjeta_personal_telefono_completo;
-                rowTef  = '                       <span style="font-size:1.0rem; color:#205aa7;"> <i class="fa fa-mobile-alt" style="color:#74b8e5;"></i>&nbsp;&nbsp;&nbsp;&nbsp;'+ element1.tarjeta_personal_telefono_completo +' </span>';
+                rowTef  = '                       <span style="color:#205aa7;"> <i class="fa fa-mobile-alt" style="color:#74b8e5;"></i>&nbsp;&nbsp;&nbsp;&nbsp;'+ element1.tarjeta_personal_telefono_completo +' </span>';
             }  
         });
 
@@ -51,30 +51,38 @@ function getVCARD(codDoc){
                 '           <div class="row">'+
                 '               <div class="col-sm-12">'+
                 '                   <div class="form-group" style="margin-bottom:0px;">'+
-                `                       <img src="../assets/images/logo_index_3.png" style="background-position:center; background-repeat:no-repeat; background-size:contain; height:200px;" />`+
+                `                       <img src="../assets/images/logo_index_3.png" style="background-position:center; background-repeat:no-repeat; background-size:contain; height:100px;" />`+
                 '                   </div>'+
                 '               </div>'+
                 ''+
                 '               <div class="col-sm-12">'+
                 '                   <div class="form-group">'+
-                '                       <span style="font-size:2.0rem; color:#205aa7; font-weight: bold;"> '+ element.tarjeta_personal_nombre +' </span>' +
+                '                       <span style="font-size:1.50rem; color:#205aa7; font-weight: bold;"> '+ element.tarjeta_personal_nombre +' </span>' +
                 '<br>'+
-                '                       <span style="font-size:1.0rem; color:#205aa7;"> '+ element.tipo_cargo_nombre +' </span>' +
+                '                       <span style="font-size:1rem; color:#205aa7;"> '+ element.tipo_cargo_nombre +' </span>' +
                 '                   </div>'+
                 '               </div>'+
                 ''+
-                '               <div class="col-sm-12" style="margin-bottom:1rem;">'+
-                '                   <div class="form-group">'+
-                '                       <span style="font-size:1.0rem; color:#205aa7;"> <i class="fa fa-envelope" style="color:#74b8e5;"></i>&nbsp;&nbsp;&nbsp;'+ element.tarjeta_personal_email +' </span>' +
+                '               <div class="col-sm-12" style="margin-bottom:0px;">'+
+                '                   <div class="form-group" style="font-size:0.75rem !important;">'+
+                '                       <span style="color:#205aa7;"> <i class="fa fa-envelope" style="color:#74b8e5;"></i>&nbsp;&nbsp;&nbsp;'+ element.tarjeta_personal_email +' </span>' +
                 '<br>'+
-                '                       <span style="font-size:1.0rem; color:#205aa7;"> <i class="fa fa-phone" style="color:#74b8e5; transform:rotate(90deg);"></i>&nbsp;&nbsp;&nbsp;+595 215172000 </span>' +
+                '                       <span style="color:#205aa7;"> <i class="fa fa-phone" style="color:#74b8e5; transform:rotate(90deg);"></i>&nbsp;&nbsp;&nbsp;+595 215172000 </span>' +
                 '<br>' + rowTef +
                 '                   </div>'+
                 '               </div>'+
                 ''+
-                '               <div class="col-sm-12">'+
+                '               <div class="col-sm-12" style="margin-bottom:1rem;">'+
                 '                   <div class="form-group" style="margin-bottom:0px;">'+
                 '                       <img src="https://api.qrserver.com/v1/create-qr-code/?data='+ encodeURIComponent(rowVCARD) +'&size=200x200&color=32-90-167" />'+
+                '                   </div>'+
+                '               </div>'+
+                ''+
+                '               <div class="col-sm-12">'+
+                '                   <div class="form-group">'+
+                '                       <span style="font-size:0.75rem; color:#205aa7; text-align:center; padding:0px 90px; display:block;"> Autopista Silvio Pettirossi y Valois Rivarola. Luque, Paraguay. </span>' +
+                '<br>'+
+                '                       <span style="font-size:1rem; color:#205aa7; font-weight:bold;"> www.conmebol.com </span>' +
                 '                   </div>'+
                 '               </div>'+
                 '           </div>'+
@@ -96,10 +104,10 @@ function getVCARD(codDoc){
                 '               </div>'+
                 ''+
                 '               <div class="col-sm-12" style="margin-bottom:1rem;">'+
-                '                   <div class="form-group">'+
-                '                       <span style="font-size:1.0rem; color:#205aa7;"> <i class="fa fa-envelope" style="color:#74b8e5;"></i>&nbsp;&nbsp;&nbsp;'+ element.tarjeta_personal_email +' </span>' +
+                '                   <div class="form-group" style="font-size:1.0rem;">'+
+                '                       <span style="color:#205aa7;"> <i class="fa fa-envelope" style="color:#74b8e5;"></i>&nbsp;&nbsp;&nbsp;'+ element.tarjeta_personal_email +' </span>' +
                 '<br>'+
-                '                       <span style="font-size:1.0rem; color:#205aa7;"> <i class="fa fa-phone" style="color:#74b8e5; transform:rotate(90deg);"></i>&nbsp;&nbsp;&nbsp;+595 215172000 </span>' +
+                '                       <span style="color:#205aa7;"> <i class="fa fa-phone" style="color:#74b8e5; transform:rotate(90deg);"></i>&nbsp;&nbsp;&nbsp;+595 215172000 </span>' +
                 '<br>' + rowTef +
                 '                   </div>'+
                 '               </div>'+
